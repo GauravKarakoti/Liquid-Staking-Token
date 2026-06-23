4a252b41c0402dc5a8e376f2bda6d1f02f13f5db
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-06-23 13:10:03 +0530

    Delete dist directory

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-// import express from "express";
-// import { burnTokens, mintTokens, sendNativeTokens } from "./mintTokens";
